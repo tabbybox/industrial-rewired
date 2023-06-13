b045bda79c146193c64fa300daab871bd3e77c06
Mio Nekoya <admin@mio9.sh>
2023-06-14 04:02:08 +0900

    dont know changed what 7

--- src/main/java/art/tabbybox/industrialrewired/IndustrialRewired.java
+++ src/main/java/art/tabbybox/industrialrewired/IndustrialRewired.java
@@ -32,6 +32,7 @@ public class IndustrialRewired {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
+
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
